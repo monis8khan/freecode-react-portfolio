a118da767af57146ac32bd63a1d7c8c4c9c3007b
monis8khan@gmail.com <monis8khan@gmail.com>
2022-09-20 16:35:10 +0500

    portfolio page completed

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -2,26 +2,57 @@ import React, { useEffect, useState } from 'react'
 import Loader from 'react-loaders'
 import AnimatedLetters from '../AnimatedLetters'
 import "./index.scss"
+import portfolioData from "../../data/portfolio.json"
 
 const Portfolio = () => {
     const [letterClass, setLetterClass] = useState('text-animate');
     let heading = "Portfolio"
+    console.log(portfolioData)
 
-    const renderPortfolio=()=>{
-        //-- watched till 25mins this function wasn't defined in the tutorial yet.......
-    }//
     useEffect(() => {
-        setTimeout(() => {
+
+        const timer = setTimeout(() => {
             return setLetterClass('text-animate-hover')
-        }, 3000)
-    }, [])
+        }, 3000);
+
+        return () => {
+            clearTimeout(timer)
+        }
+    }, []);
+
+
+    const renderPortfolio = (portfolio) => { //-- functional local component
+
+        return (
+            <div className='images-container'>
+                {
+                    portfolio.map((port, idx) => {
+                        return (
+                            <div className='image-box' key={port.id}>
+                                <img src={port.cover} alt={port.title} className="portfolio-image"/>
+                                <div className='content'>
+                                    <p className='title'>{port.title}</p>
+                                    <h4 className='description'>{port.description}</h4>
+                                    <button className='btn' onClick={()=>{window.open(port.url)}}>View</button>
+                                </div>
+                            </div>
+                        )
+                    })
+                }
+            </div>
+        )
+
+    }
+
+
+
     return (
         <>
             <div className='container portfolio-page'>
                 <h1 className='page-title'>
                     <AnimatedLetters strArray={heading.split('')} idx={15} lettersClass={letterClass} />
                 </h1>
-                <div>{renderPortfolio()}</div>
+                <div>{renderPortfolio(portfolioData.Portfolio)}</div>
             </div>
             <Loader type='pacman' />
         </>
